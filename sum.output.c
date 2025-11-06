c8ed3a1e8e2f716ac59f9e6b6cb076a74e9c4f50
PRIYANSHU RAJ <priyanshu.raj25@gcu.edu.in>
2025-11-06 23:43:01 +0530

    Add files via upload

---
+++ sum.output.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a,b,sum;
+    printf("entre first number");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum = %d",sum);
+    return 0;
+}
